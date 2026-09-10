0a5e335edb6289e1348bddc7194d1e84b759bfd5
teojunkeat1 <2602560@sit.singaporetech.edu.sg>
2026-09-10 13:44:08 +0800

    Debugging

--- auditor.py
+++ auditor.py
@@ -6,20 +6,18 @@ while loop != "quit" or inventory > 500:
     stockvalue = input("What is the current stock value: ")
     #integer as input
     if stockvalue.isdigit() == False:
-        print("Error, please input a number.")
-        rejectedentries += 1
-    elif int(stockvalue) < 0:
-        print("Error, please input a positive number")
+        print("Error, please input a positive whole number.")
         rejectedentries += 1
     else:
+        stockvalue = int(stockvalue)
         inventory += stockvalue 
-    print("Your current inventory is:" + inventory)
-    loop = ("Continue? Type "continue" or "quit": ")
+    print("Your current inventory is:" + str(inventory))
+    loop = input("Continue? Type continue or quit: ")
 if inventory > 500:
     print("Current inventory is above 500.")
 elif loop == "quit":
-    print("Total Units processed: " + inventory)
-    print ("Total rejected units" + rejectedentries)
+    print("Total Units processed: " + str(inventory))
+    print ("Total rejected units: " + str(rejectedentries))
     
     
 
