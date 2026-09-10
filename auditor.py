c0fcb73490455e864583592966346312428302ca
teojunkeat1 <2602560@sit.singaporetech.edu.sg>
2026-09-10 13:32:59 +0800

    Manage state

--- auditor.py
+++ auditor.py
@@ -1,15 +1,22 @@
 inventory = 0
 loop = "continue"
+rejectedentries = 0
 while loop != "quit":
     #loop code
     stockvalue = input("What is the current stock value: ")
     #integer as input
     if stockvalue.isdigit() == False:
         print("Error, please input a number.")
+        rejectedentries += 1
+    elif int(stockvalue) < 0:
+        print("Error, please input a positive number")
+        rejectedentries += 1
     else:
-        stockvalue = int(stockvalue)
-        if stockvalue < 0:
-            print("Error, please input a positive number")
-            
+        inventory += stockvalue 
+    print("Your current inventory is:" + inventory)
+    loop = ("Continue? Type "continue" or "quit": ")
+
+    
+
 
 
